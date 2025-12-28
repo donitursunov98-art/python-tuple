eff6506aa51da215fdb2e4b60bbb7a6667dbe9f7
Abu <efootball042605@gmail.com>
2025-12-28 19:38:56 +0500

    1-mashq

---
+++ main1.py
@@ -0,0 +1,9 @@
+people = [("Ali", 24), ("Laylo", 30), ("Jasur", 19)]
+
+max_old_people = people[0]
+
+for person in people:
+    if person[1] > max_old_people[1]:
+        max_old_people = person
+
+print(max_old_people)
